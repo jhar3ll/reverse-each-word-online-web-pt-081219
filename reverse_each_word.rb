5dad9226badc29171877850a2e6938d683ae7910
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-14 23:05:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
  string.split.collect do |word|
-   word.reverse 
+   word.reverse.join(" ")
 end
   
- end 
+  
   
 reverse_each_word("Hello there, and how are you?")
